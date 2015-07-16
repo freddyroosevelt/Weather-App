94eb11d6a4ec58caa5ea5f727b404a78af174f0c
Freddy Roosevelt <rooseveltfreddy@gmail.com>
2015-07-16 14:04:43 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    host = '127.0.0.1',
+    port = '9000';
+
+var mimes = {
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "text/javascript",
+    ".gif" : "image/gif",
+    ".jpg" : "image/jpg",
+    ".png" : "image/png"
+}
+
+var server = http.createServer(function(req, res) {
+    var filePath = (req.url === '/') ? ('./index.html') : ('.' + req.url);
+    var contentType = mimes[path.extname(filePath)];
+
+    // Check to see if the file exists or not
+    fs.exists(filePath, function(file_exists) {
+        if(file_exists) {
+
+            // Read and Serve in a more Efficient way using Streams
+            res.writeHead(200, { 'Content-Type' : contentType});
+            var streamFile = fs.createReadStream(filePath).pipe(res);
+
+            // event listener for possible errors
+            streamFile.on('err', function() {
+                res.writeHead(500);
+                res.end();
+            })
+
+        }else {
+            res.writeHead(404);
+            res.end("Sorry we could not find the file you requested!");
+        }
+    })
+
+}).listen(port, host, function(){
+    console.log('Server Running on http://' + host + ':' + port);
+})
---
+++ services.js
@@ -0,0 +1,11 @@
+// SERVICES
+weatherApp.service('cityService', function() {
+    this.city = "Los Angeles, CA";
+});
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+    this.GetWeather = function(city, days) {
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP"}});
+        return weatherAPI.get({ q: city, cnt: days});
+    };
+}]);
